e216f30e140a602aecee5a1e3f0255e66b194584
deiviz <tangerdream@gmail.com>
2020-09-23 12:12:34 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
+// variable que contendrá el registro del sw
+var swRegistry;
+
+
+if ( navigator.serviceWorker ) {
+    if ( url.includes('localhost') ) {
+        swLocation = '/sw.js';
+    }
+
+    // ESPERAMOS QUE SE CARGUE TODO PARA REGISTAR EL SW
+    // YA QUE COMO EL SW HACE MUCHAS COSAS POR DEBAJO, 
+    // PUEDE COMPROMETER EL RENDIMIENTO INICIAL DE NUESTRA APP
+    window.addEventListener("load", function () {
+        navigator.serviceWorker.register( swLocation )
+            .then(reg => {
+                swRegistry = reg
+                // Comprobar si ya estoy suscrito a las notificaciones o no
+                swRegistry.pushManager.getSubscription()
+                    .then( verificaSuscripcion )
+            })
+    })
+}
+
+
+function verificaSuscripcion () {
+  console.log('verifica')
+}
---
+++ sw.js
@@ -0,0 +1,39 @@
+
+self.addEventListener('install', event => {
+  console.log('isntalling', event)
+
+  const shellCache = caches.open('caché-shell-1')
+    .then( cache => {
+      return cache.addAll([
+        '/',
+        '/manifest.json',
+        '/img/favicon.ico',
+        '/index.html',
+        '/css/app.css',
+        '/img/ico1.png',
+        '/img/ico2.png',
+        '/img/ico3.png',
+        '/img/ico4.png',
+        '/img/img1.png',
+        '/img/img2.webp',
+        '/img/img3.webp',
+        '/js/app.js'
+      ])
+    })
+  event.waitUntil(shellCache)
+})
+
+self.addEventListener('activate', event => {
+  console.log('activated', event)
+})
+
+self.addEventListener('fetch', event => {
+  // caché only strategy
+  // event.respondWith(caches.match(event.request))
+
+  // cache with network fallback
+  const resp = caches.match( event.request)
+    .then(res => res ? res : fetch(event.request))
+
+  event.respondWith( resp )
+})
