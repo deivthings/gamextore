90935a313e4e09964a179177b92925344d522a30
deiviz <tangerdream@gmail.com>
2021-10-05 17:18:40 +0200

    beforeinsallprompt control

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
 
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+var btnInstallPWA = document.getElementById("btn-install-pwa");
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  // showInstallPromotion();
+  btnInstallPWA.style.display = "block"
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.!!`);
+});
+
+btnInstallPWA.addEventListener('click', async () => {
+  // Hide the app provided install promotion
+  // hideInstallPromotion();
+  btnInstallPWA.style.display = "none";
+  // Show the install prompt
+  deferredPrompt.prompt();
+
+  // Wait for the user to respond to the prompt
+  const { outcome } = await deferredPrompt.userChoice;
+  // Optionally, send analytics event with outcome of user choice
+  console.log(`User response to the install prompt: ${outcome}`);
+  // We've used the prompt, and can't use it again, throw it away
+  deferredPrompt = null;
+});
+
+
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
       .register("sw.js")
